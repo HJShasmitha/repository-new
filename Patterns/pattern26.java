59c7d5656aa967214199be6f03446d8f68fa3c46
HJ Shasmitha Hariharan <139241561+HJShasmitha@users.noreply.github.com>
2025-04-25 13:44:52 +0530

    Add files via upload

---
+++ Patterns/pattern26.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class pattern26 {
+    public static void main(String[] args) {
+        char i ;
+        char j;
+
+        for (i = 'A'; i <= 'E' ;i++)
+        {
+            for (j = 'A';j <= 'E';j++)
+            {
+                System.out.print(i + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ Patterns/pattern29.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class pattern29 {
+    public static void main(String[] args) {
+        char i;
+        char j;
+
+        for(i ='E';i >= 'A'; i--)
+        {
+            for(j = 'E'; j >= 'A' ; j--){
+                System.out.print(j +" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
